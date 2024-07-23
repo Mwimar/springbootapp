c1b1336de52978685c5cb3dab2f7f340771ed561
Mark Mwima <markmwimar@gmail.com>
2024-07-23 23:09:01 +0300

    logger

--- src/main/java/com/application/application/Application.java
+++ src/main/java/com/application/application/Application.java
@@ -1,16 +1,20 @@
 package com.application.application;
 
+import lombok.extern.flogger.Flogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
+private static final Logger log = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
-		System.out.println(welcomeMessage);
+		SpringApplication.run(Application.class, args);
+		log.info("Application is up and Running!!");
+
+
 
 
 	}
