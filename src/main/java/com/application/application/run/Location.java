620a5e83ab9dd387e8ba2380f20b7958e68dfe02
Mark Mwima <markmwimar@gmail.com>
2024-07-24 00:00:45 +0300

    public enum

---
+++ src/main/java/com/application/application/run/Location.java
@@ -0,0 +1,5 @@
+package com.application.application.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
--- src/main/java/com/application/application/run/Run.java
+++ src/main/java/com/application/application/run/Run.java
@@ -1,6 +1,11 @@
 package com.application.application.run;
-
-public record Run{
-
+import java.time.LocalDateTime;
+public record Run (
+        Integer id,
+        String title,
+        LocalDateTime startedOn,
+        LocalDateTime endedOn,
+        Integer miles,
+        Location location ){
 
 }
