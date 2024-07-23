f079426aa1825aeefd1926685929c1723bf5e64c
Mark Mwima <markmwimar@gmail.com>
2024-07-23 23:40:40 +0300

    run class getters

--- src/main/java/com/application/application/run/Run.java
+++ src/main/java/com/application/application/run/Run.java
@@ -1,4 +1,26 @@
 package com.application.application.run;
 
+import java.time.LocalDateTime;
+
 public class Run {
+    private Integer id;
+    private String title;
+    private LocalDateTime startedOn;
+    private LocalDateTime completedOn;
+    private Integer miles;
+    private Location location;
+
+    //Constructor
+    public Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles, Location location){
+        this.id=id;
+        this.title=title;
+        this.startedOn=startedOn;
+        this.completedOn=completedOn;
+        this.miles=miles;
+        this.location=location;
+    }
+
+
+
+
 }
