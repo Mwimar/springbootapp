39d8b5eb8d5d43fe19dc9fc3de94155bba641795
Mark Mwima <markmwimar@gmail.com>
2024-07-24 12:51:14 +0300

    Get mapping

--- src/main/java/com/application/application/run/RunController.java
+++ src/main/java/com/application/application/run/RunController.java
@@ -1,8 +1,13 @@
 package com.application.application.run;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RunController {
 
+    @GetMapping("/hello")
+   String Home (){
+     return "Hi Runners";
+ }
 }
