a2339464b3d824947d40cc4a56d73bca70efd511
Mark Mwima <markmwimar@gmail.com>
2024-07-24 13:16:43 +0300

    returning list of runs method

--- src/main/java/com/application/application/run/RunController.java
+++ src/main/java/com/application/application/run/RunController.java
@@ -3,11 +3,13 @@ package com.application.application.run;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class RunController {
 
-    @GetMapping("/hello")
-   String Home (){
-     return "Hi Runners";
- }
+List<Run> findAll(){
+    return null;
+}
+
 }
