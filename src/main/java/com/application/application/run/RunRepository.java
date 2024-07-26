78254bdeda800f41fec2b82f1b755a4fd3ea822e
Mark Mwima <markmwimar@gmail.com>
2024-07-26 23:56:08 +0300

    adding a run

--- src/main/java/com/application/application/run/RunRepository.java
+++ src/main/java/com/application/application/run/RunRepository.java
@@ -20,6 +20,12 @@ public class RunRepository {
     Optional <Run> findById(Integer id){
         return runs.stream().filter(run -> run.id()==id).findFirst();
     };
+
+
+    void create (Run run){
+        runs.add(run);
+    }
+
 @PostConstruct
     private void init(){
         runs.add(new Run(1,
